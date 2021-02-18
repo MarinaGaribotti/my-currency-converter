3fe76e4648793e184ef78917093e262ace637c97
MarinaGaribotti <marinagaribotti@hotmail.com>
2021-02-18 22:15:59 +0100

    currencies dynamic data done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,12 +6,13 @@ import Header from '../Header';
 import Currencies from '../Currencies';
 import Result from '../Result';
 import './styles.css';
+import currenciesList from '../../data/currencies';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header baseAmount={12} />
-    <Currencies />
+    <Currencies currenciesList={currenciesList} />
     <Result value={1.09} currency="United States Dollar"/>
   </div>
 );
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,27 +1,37 @@
 // imports npm
 import React from 'react';
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 // mes imports
 import './style.scss';
 
 // mon composant
-const Currencies = () => (
+const Currencies = ({ currenciesList }) => (
   <div className="currencies">
     <div className="currencies-title">
       Currencies
     </div>
     <ul className="currencies-list">
-      <li className="currency">Devise 1</li>
-      <li className="currency">Devise 2</li>
-      <li className="currency">Devise 3</li>
-      <li className="currency">Devise 4</li>
-      <li className="currency">Devise 5</li>
-      <li className="currency">Devise 5</li>
+      {
+        currenciesList.map((currencyObject) => (
+          <li key={currencyObject.name} className="currency">
+            {currencyObject.name}
+          </li>
+        ))
+      }
     </ul>
   </div>
-
 );
+
+// mes proptypes
+Currencies.propTypes = {
+  currenciesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+
+};
 
 // export du composant pour qu'il soit accessible
 export default Currencies;
