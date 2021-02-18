09594d92dfb4c6b85d1afc02ab227f161bf8bb67
MarinaGaribotti <marinagaribotti@hotmail.com>
2021-02-18 18:34:49 +0100

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+  </div>
+);
+
+// == Export
+export default App;
